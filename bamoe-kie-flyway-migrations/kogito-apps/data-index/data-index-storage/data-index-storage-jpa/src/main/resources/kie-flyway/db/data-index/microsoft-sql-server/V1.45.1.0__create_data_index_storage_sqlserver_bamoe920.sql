61acc1d17a1b8443622b61714f643397b1dc7848
Abdul Jaleel AK <abduljaleelak@gmail.com>
2025-02-27 16:22:41 +0530

    removed definitions_metadata table as it removed from upstream
    Signed-off-by: Abdul Jaleel AK <abduljaleelak@gmail.com>

--- bamoe-kie-flyway-migrations/kogito-apps/data-index/data-index-storage/data-index-storage-jpa/src/main/resources/kie-flyway/db/data-index/microsoft-sql-server/V1.45.1.0__create_data_index_storage_sqlserver_bamoe920.sql
+++ bamoe-kie-flyway-migrations/kogito-apps/data-index/data-index-storage/data-index-storage-jpa/src/main/resources/kie-flyway/db/data-index/microsoft-sql-server/V1.45.1.0__create_data_index_storage_sqlserver_bamoe920.sql
@@ -27,16 +27,6 @@ CREATE TABLE definitions_annotations (
     CONSTRAINT fk_definitions_annotations FOREIGN KEY (process_id, process_version) REFERENCES process_definitions(id, version) ON DELETE CASCADE
 );
 
-CREATE TABLE definitions_metadata (
-    process_id character varying(255) NOT NULL,
-    process_version character varying(255) NOT NULL,
-    meta_value character varying(255),
-    name character varying(255) NOT NULL,
-    CONSTRAINT definitions_metadata_pkey PRIMARY KEY (process_id, process_version, name),
-    CONSTRAINT fk_definitions_metadata FOREIGN KEY (process_id, process_version) REFERENCES process_definitions(id, version) ON DELETE CASCADE
-
-);
-
 CREATE TABLE definitions_nodes (
     id character varying(255) NOT NULL,
     name character varying(255),
@@ -237,8 +227,6 @@ CREATE INDEX idx_attachments_tid ON attachments (task_id)
 CREATE INDEX idx_definitions_addons_pid_pv ON definitions_addons (process_id, process_version);
 
 CREATE INDEX idx_definitions_annotations_pid_pv ON definitions_annotations (process_id, process_version);
-
-CREATE INDEX idx_definitions_metadata_pid_pv ON definitions_metadata (process_id, process_version);
 
 CREATE INDEX idx_definitions_nodes_metadata_pid_pv ON definitions_nodes_metadata (process_id, process_version);
 
