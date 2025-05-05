222f87288039084c4e666ddd9d457f833439fd24
soniyaabraham <soniya.abraham@ibm.com>
2025-05-05 20:00:52 +0530

    Adding the counters funtionality scripts
    Signed-off-by: soniyaabraham <soniya.abraham@ibm.com>

--- bamoe-kie-flyway-migrations/kogito-apps/data-index/data-index-storage/data-index-storage-jpa/src/main/resources/kie-flyway/db/data-index/microsoft-sql-server/V1.45.1.1__create_data_index_storage_sqlserver_bamoe921.sql
+++ bamoe-kie-flyway-migrations/kogito-apps/data-index/data-index-storage/data-index-storage-jpa/src/main/resources/kie-flyway/db/data-index/microsoft-sql-server/V1.45.1.1__create_data_index_storage_sqlserver_bamoe921.sql
@@ -2,4 +2,4 @@ ALTER TABLE nodes ADD retrigger BIT DEFAULT 0;
 
 ALTER TABLE nodes ADD rror_message character varying(max);
 
-ALTER TABLE processes ADD node_instance_id VARCHAR(255);
+ALTER TABLE processes ADD node_instance_id character varying(255);
